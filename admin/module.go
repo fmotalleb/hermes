137750106940b9f73a696d7c7bd1c5db987a847f
Motalleb Fallahnezhad <fmotalleb@gmail.com>
2026-05-15 21:08:17 +0330

    fix: fallback route would be count as `{id}`

--- admin/module.go
+++ admin/module.go
@@ -16,7 +16,7 @@ func Register(app *gofr.App) {
 	app.POST("/admin/inbound/{id}", handler.updateInboundEntrypoint)
 	app.POST("/admin/inbound/{id}/delete", handler.deleteInboundEntrypoint)
 	app.POST("/admin/forward-zones", handler.createForwardZone)
+	app.POST("/admin/forward-zones/fallback", handler.updateFallbackForwardZone)
 	app.POST("/admin/forward-zones/{id}", handler.updateForwardZone)
 	app.POST("/admin/forward-zones/{id}/delete", handler.deleteForwardZone)
-	app.POST("/admin/forward-zones/fallback", handler.updateFallbackForwardZone)
 }
